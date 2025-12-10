9c0e0f32319e019d261fac99765da28354ade8c0
CoreJK <r465755956@gmail.com>
2025-12-10 17:25:50 +0800

    📙 docs: 添加项目描述文档

--- DSA/Head_vs_tall.py
+++ DSA/Head_vs_tall.py
@@ -1,1 +1,29 @@
-from 
+from functools import wraps
+import time
+
+def time_count(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)        
+        end_time = time.time()
+        print(f"use time: {end_time - start_time}")
+        return result
+    return wrapper
+
+@time_count
+def test_append():
+    empty_list = []
+    for i in range(50000):
+        empty_list.append(i)
+        
+
+@time_count
+def test_insert():
+    empty_list = []
+    for i in range(50000):
+        empty_list.insert(0, i)
+
+test_append()
+
+test_insert()
